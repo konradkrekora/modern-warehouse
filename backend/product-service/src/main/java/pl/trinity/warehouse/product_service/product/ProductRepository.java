f499616b57caed293a99afd01d5ffac013a57454
Konrad <konradkrekora33@gmail.com>
2026-05-12 23:41:46 +0200

    simplyfying repository

--- backend/product-service/src/main/java/pl/trinity/warehouse/product_service/product/ProductRepository.java
+++ backend/product-service/src/main/java/pl/trinity/warehouse/product_service/product/ProductRepository.java
@@ -3,11 +3,5 @@ package pl.trinity.warehouse.product_service.product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
-public interface ProductRepository extends JpaRepository<Product, Long> {
-    List<Product> findAll();
-
-    Product save(Product product);
-}
+public interface ProductRepository extends JpaRepository<Product, Long> {}
