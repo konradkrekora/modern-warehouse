265d5b8fe55c2f45febf88fd75a48fb3cfd5c9d5
Konrad <konradkrekora33@gmail.com>
2026-05-15 22:18:16 +0200

    warehouse service first commit

---
+++ backend/warehouse-service/src/test/java/pl/trinity/warehouse/warehouse_service/WarehouseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.trinity.warehouse.warehouse_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
