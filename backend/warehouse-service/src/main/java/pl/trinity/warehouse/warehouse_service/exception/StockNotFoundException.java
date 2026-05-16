78623a08a1c0c333e20258662dee8d612f91bc5c
Konrad <konradkrekora33@gmail.com>
2026-05-16 20:04:30 +0200

    stock not found exception

---
+++ backend/warehouse-service/src/main/java/pl/trinity/warehouse/warehouse_service/exception/StockNotFoundException.java
@@ -0,0 +1,7 @@
+package pl.trinity.warehouse.warehouse_service.exception;
+
+public class StockNotFoundException extends RuntimeException {
+    public StockNotFoundException(String sku) {
+        super("Stock with sku " + sku + "does not exist");
+    }
+}
