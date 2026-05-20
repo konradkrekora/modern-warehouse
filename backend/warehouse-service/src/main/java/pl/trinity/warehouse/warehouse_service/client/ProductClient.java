25a77ebfd18c3065b167ed69d6634d1929231419
Konrad <konradkrekora33@gmail.com>
2026-05-20 21:52:30 +0200

    load balance configuration and some fixes

--- backend/warehouse-service/src/main/java/pl/trinity/warehouse/warehouse_service/client/ProductClient.java
+++ backend/warehouse-service/src/main/java/pl/trinity/warehouse/warehouse_service/client/ProductClient.java
@@ -3,17 +3,16 @@ package pl.trinity.warehouse.warehouse_service.client;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import pl.trinity.warehouse.warehouse_service.dto.ProductDto;
 
-@FeignClient(name = "product-service", url = "http://localhost:8081/api/products")
+@FeignClient(name = "product-service")
 public interface ProductClient {
 
     // Ta metoda musi idealnie odpowiadać endpointowi z ProductController
-    @GetMapping("/{id}")
+    @GetMapping("/api/products/{id}")
     Object getProductById(@PathVariable("id") Long id);
 
     // product-service ma endpoint: GET /api/products/search?sku=XYZ
-    @GetMapping("/sku/{sku}")
+    @GetMapping("/api/products/sku/{sku}")
     ProductDto getProductBySku(@PathVariable("sku") String sku);
 }
